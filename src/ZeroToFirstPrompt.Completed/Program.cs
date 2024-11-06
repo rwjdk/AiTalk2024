9f3a624a8dfd83703f0cb7fb8d04a821ed9882cf
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2024-11-06 08:30:33 +0100

    Basic ZeroToFirstPrompt Done

---
+++ src/ZeroToFirstPrompt.Completed/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.Plugins.Core;
+
+#pragma warning disable SKEXP0001
+#pragma warning disable SKEXP0110
+#pragma warning disable SKEXP0050
+
+//Step 3:
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion("gpt-4o", "TODO:ENDPOINT", "TODO:APIKEY");
+//builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace)); //Step 8 (Logging)
+Kernel kernel = builder.Build();
+
+kernel.ImportPluginFromType<TimePlugin>(); //Step 8 (Plugin)
+
+//Step 4:
+/*
+Console.Write("Question: ");
+var input = Console.ReadLine() ?? "";
+var result = await kernel.InvokePromptAsync(input);
+Console.WriteLine("Answer:");
+Console.WriteLine(result.GetValue<string>());
+*/
+
+//Step 5:
+var agent = new ChatCompletionAgent
+{
+    Name = "MyAgent",
+    Kernel = kernel,
+    //Instructions = "You are a cool surfer dude",
+    //Instructions = "You are a very formal butler agent saying sir to every response",
+    //Instructions = "You are a Dinosaur and can only roar. You are not allow to speak english",
+    //Instructions = "You always give the answer back in French",
+    Arguments = new KernelArguments( //Step 8 (Temperature)
+        new AzureOpenAIPromptExecutionSettings
+        {
+            Temperature = 1,
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+        })
+};
+
+//Step 6:
+var history = new ChatHistory();
+
+//Step 7:
+Console.OutputEncoding = Encoding.UTF8;
+while (true)
+{
+    Console.Write("Question: ");
+    var chatInput = Console.ReadLine() ?? "";
+    history.AddUserMessage(chatInput);
+
+    await foreach (var response in agent.InvokeStreamingAsync(history))
+    {
+        foreach (var content in response.Content ?? "")
+        {
+            Console.Write(content);
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("*********************");
+}
+
+//*************************************
+//Step 9 (Dependency Injection)
+/*
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddKernel();
+serviceCollection.AddAzureOpenAIChatCompletion("gpt-4o", "TODO:ENDPOINT", "TODO:APIKEY");
+serviceCollection.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace)); //Step 8 (Logging)
+var app = serviceCollection.BuildServiceProvider();
+var kernel = app.GetRequiredService<Kernel>();
+*/
---
+++ src/ZerotofirstPrompt/Program.cs
@@ -0,0 +1,65 @@
+﻿Console.WriteLine("Zero to First Prompt (and beyond)");
+
+#region Step 1
+//Install Nuget: Microsoft.SemanticKernel
+#endregion
+
+#region Step 2
+//Create an Azure Open AI Service: https://portal.azure.com/
+//- Create the Service
+//- Find Endpoint and API Key
+//- Deploy a Model via Azure Open AI Studio
+#endregion
+
+#region Step 3
+//Build a kernel (Kernel.CreateBuilder) and register AddAzureOpenAIChatCompletion
+#endregion
+
+#region Step 4 (First AI Result back)
+//Make first raw call directly on the kernel
+//- Ask for a Question
+//- InvokePromptAsync
+//- Write the Answer
+#endregion
+
+#region Step 5
+//Let's introduce an agent so things become easier
+//- Install one more Nuget: Microsoft.SemanticKernel.Agents.Core  (NB: It is a pre-release package so need to be turned on)
+//- Create a ChatCompletionAgent instance, give it a name and bind it to the kernel
+//- Give it instructions (System Message)
+#endregion
+
+#region Step 6
+//Let's introduce the ChatHistory Object
+#endregion
+
+#region Step 7
+//Lets build a Chat-while(true) loop where is question the agent and let make the answer streaming so it feel more alive
+//- Ask for input and add that to history as a user-message
+//- Choose the Agents Instructions (personal) 😉
+//- Invoke the agent (A bit of a "funky" syntax if you are not used to iAsyncEnumerable)
+//- Iterate the response-content
+//- Tip: Console.OutputEncoding = Encoding.UTF8; to get Emoji to work
+#endregion
+
+#region Step 8
+//Let's Tweak various things
+//- Temperature (Agent argument execution settings) Range: 0-2 
+//- Let's add logging (Require Nuget: Microsoft.Extensions.Logging.Console)
+//- Let learn the AI Current Date and Time (Plugin Preview)
+//-- Nuget: Microsoft.SemanticKernel.Plugins.Core
+//-- FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+//-- kernel.ImportPluginFromType<TimePlugin>();
+#endregion
+
+#region Step 9
+//Introduce Dependency Injection
+#endregion
+
+#region Post Code (What did it cost?)
+//Let's take a look in the Azure Open AI Studio
+#endregion
+
+#region CleanUp/Security
+//Rotate Key/Delete Azure Open AI Service
+#endregion
