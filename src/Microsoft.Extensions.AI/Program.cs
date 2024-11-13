b279c6dc80247a0ad0e2d13ba0409af2b74d34fe
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2024-11-13 11:04:33 +0100

    WIP

---
+++ src/Microsoft.Extensions.AI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ClientModel;
+using System.Text;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using Shared;
+
+var azureOpenAiCredentials = SecretManager.GetAzureOpenAiCredentials();
+
+AzureOpenAIClient azureOpenAiClient = new AzureOpenAIClient(
+    new Uri(azureOpenAiCredentials.Endpoint),
+    new ApiKeyCredential(azureOpenAiCredentials.ApiKey));
+
+IChatClient client = azureOpenAiClient.AsChatClient(modelId: "gpt-4o-mini");
+Console.OutputEncoding = Encoding.UTF8;
+while (true)
+{
+
+    Console.Write("Question: ");
+    var question = Console.ReadLine() ?? "";
+    await foreach (var update in client.CompleteStreamingAsync(question, new ChatOptions
+                   {
+                       ToolMode = ChatToolMode.Auto
+                   }))
+    {
+        Console.Write(update.Text);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("*********************");
+    Console.WriteLine();
+}
